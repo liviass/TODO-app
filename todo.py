75a00c04973c0b0a918e2034928583ee810d7a0b
Lívia Simões <liviazinhass@gmail.com>
2018-04-14 16:30:39 -0300

    integração contínua

--- todo.py
+++ todo.py
@@ -1,8 +1,7 @@
 from flask import Flask, jsonify
 
 app = Flask('todoapp')
-tarefas = [{'id': 1, 'titulo': 'titulo', 'descricao': 'descricao da tarefa',
-            'estado': False}]
+tarefas = []
 
 @app.route('/task')
 def listar():
